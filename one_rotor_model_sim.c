445a986757380b8e37e7990001d9d25a8e7eef38
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2022-09-10 21:17:15 +0900

    update

--- one_rotor_model_sim.c
+++ one_rotor_model_sim.c
@@ -1,7 +1,7 @@
 /******************************
  *                            
  * ルンゲクッタ法による 
- * マルチコプタシミュレーション       
+ * マルチコプタの高度制御シミュレーション       
  * サンプルプログラム            
  *                            
  ******************************/
@@ -38,121 +38,43 @@ long cpu_time;
 //モータ状態構造体
 typedef struct 
 {
-  double i;
   double omega;
   double u;
-  double i_;
   double omega_;
   double u_;
 } motor_t;
 
-//
-//マルチコプタ座標系
-// 
-// 右が正、下が正
-// 反時計回りが回転の正
-//
-// (o)------------>x
-//  |
-//  |
-//  |
-//  |
-//  V
-//  z
-
 //マルチコプタ状態構造体
 typedef struct
 {
-  double q;
-  double theta;
-  double u;
   double w;
-  double x;
   double z; 
-  double q_;
-  double theta_;
-  double u_;
   double w_;
-  double x_;
   double z_; 
 } drone_t;
 
-//Equation of current
-//Lm di/dt + Rm i + Km omega = u
-//i:current
-//t:time
-//value[0]:omega
-//value[1]:u
-double i_dot(double i, double t, double *value)
-{
-  double omega = value[0];
-  double u = value[1];
-  return (u - Rm * i - Km * omega)/Lm;
-}
-
 //Motor Equation of motion
 //TL = Cq omega^2
-//Jm domega/dt + Dm omega + TL = Km i
+//Jm domega/dt + (Dm + K^2/Rm) omega + TL = Km u/R
 //omega:angular velocity
 //t:time
-//value[0]:i
 double omega_dot(double omega, double t, double *value)
 {
-  double i=value[0];
   double TL=Cq * omega * omega;
-  return (Km * i - Dm * omega - TL)/Jm;
+  return ((Dm+Km*Km/Rm)*omega + TL)/Jm;
 }
 
-//Multcopter Equation of angular motion
-//Jcpt dq/dt = (T_R-T_L) l
-//omega_R:prop angular velocity
-//omega_L:prop angular velocity
-//t:time
-//value[0]:omega_R
-//value[1]:omega_L
-double q_dot(double q, double t, double *value)
-{
-  double omega_R = value[0];
-  double omega_L = value[1];
-  double T_R = Ct * omega_R * omega_R;
-  double T_L = Ct * omega_L * omega_L;
-  return (T_R - T_L)*lcpt/Jcpt;
-}
 
 //Multcopter Equation of motion
-//Md du/dt = -(T_R+T_L) + Md*grav*sin(theta)
-//omega_R:angular velocity
-//omega_L:angular velocity
+//Md dw/dt = T - M g
+//omega:angular velocity
 //t:time
-//value[0]:omega_R
-//value[1]:omega_L
-//value[2]:theta
-double u_dot(double w, double t, double *value)
-{
-  double omega_R = value[0];
-  double omega_L = value[1];
-  double theta = value[2];
-  double T_R = Ct * omega_R * omega_R;
-  double T_L = Ct * omega_L * omega_L;
-  return -Md*Grav*sin(theta)/Md;
-}
-
-//Multcopter Equation of motion
-//Md du/dt = -(T_R+T_L) + Md*grav*sin(theta)
-//omega_R:angular velocity
-//omega_L:angular velocity
-//t:time
-//value[0]:omega_R
-//value[1]:omega_L
-//value[2]:theta
+//value[0]:omega
 double w_dot(double w, double t, double *value)
 {
-  double omega_R = value[0];
-  double omega_L = value[1];
-  double theta = value[2];
-  double T_R = Ct * omega_R * omega_R;
-  double T_L = Ct * omega_L * omega_L;
-  return (-(T_R + T_L) + Md*Grav*cos(theta))/Md;
+  double omega = value[0];
+  double T = Ct * omega * omega;
+  return (T - Md*Grav)/Md;
 }
 
 //Multicopter kinematics
@@ -166,20 +88,6 @@ double theta_dot(double theta, double t, double *value)
 }
 
 //Multicopter kinematics
-//dx/dt = u*cos(theta) + w*sin(theta)
-//t:time
-//value[0]:u
-//value[1]:w
-//value[2]:theta
-double x_dot(double x, double t, double *value)
-{
-  double u = value[0];
-  double w = value[1];
-  double theta= value[2];
-  return u*cos(theta) + w*sin(theta); 
-}
-
-//Multicopter kinematics
 //dz/dt = -u*sin(theta) + w*cos(theta)
 //t:time
 //value[0]:u
@@ -187,10 +95,8 @@ double x_dot(double x, double t, double *value)
 //value[2]:theta
 double z_dot(double z, double t, double *value)
 {
-  double u = value[0];
-  double w = value[1];
-  double theta= value[2];
-  return -u*sin(theta) + w*cos(theta); 
+  double w = value[0];
+  return w; 
 }
 
 //Runge Kutta method
@@ -225,33 +131,18 @@ double rk4(double (*dxdt)(double, double, double*), double x, double t, double h
 
 void save_state(motor_t* motor, drone_t* drone)
 {
-  for (char i=RIGHT; i<LEFT+1; i++)
-  {
-    motor[i].i_ = motor[i].i;
-    motor[i].omega_ = motor[i].omega;
-    motor[i].u_ = motor[i].u;
-  }
-  drone->q_ = drone->q;
-  drone->theta_ = drone->theta;
-  drone->u_ = drone->u;
+  motor->omega_ = motor->omega;
+  motor->u_ = motor->u;
   drone->w_ = drone->w;
-  drone->x_ = drone->x;
   drone->z_ = drone->z;
 }
 
 void print_state(double t, motor_t* motor, drone_t drone)
 {
-  printf("%11.8f %11.8f %11.8f %11.8f %11.8f %11.8f %11.8f %11.8f %11.8f %11.8f %11.8f\n",
+  printf("%11.8f %11.8f %11.8f %11.8f\n",
     t, 
-    motor[RIGHT].i, 
-    motor[LEFT].i, 
-    motor[RIGHT].omega,
-    motor[LEFT].omega,
-    drone.q,
-    drone.theta,
-    drone.u,
+    motor->omega,
     drone.w,
-    drone.x,
     drone.z
   );
 }
@@ -259,53 +150,43 @@ void print_state(double t, motor_t* motor, drone_t drone)
 void drone_sim(void)
 {
   drone_t drone;
-  motor_t motor[2];
+  motor_t motor;
 
   //state init
-  motor[RIGHT].i = 0.0;
-  motor[RIGHT].omega = 0.0;
-  motor[RIGHT].u = 7.5;
-  motor[LEFT].i  = 0.0;
-  motor[LEFT].omega  = 0.0;
-  motor[LEFT].u = 7.4;
-  drone.q = 0.0;
-  drone.theta = 0.0;
+  motor.omega = 0.0;
+  motor.u = 7.5;
+  drone.w = 0.0;
+  drone.z = 0.0;
 
   double t       = 0.0; //time
-  double h       = 0.0001;//step size
+  double step    = 0.0001;//step size
+  double h = 0.02; //control period
 
   //initial state output
-  print_state(t, motor, drone);
+  print_state(t, &motor, drone);
   
+  //Simulation loop
   cpu_time = clock();
   while(t < End_time )    
   {
     //Save state
-    save_state(motor, &drone);
+    save_state(&motor, &drone);
 
     //Update(Runge-Kutta method)
-    for (char i=RIGHT; i<LEFT+1; i++)
-    {
-      motor[i].i = rk4(i_dot, motor[i].i_, t, h, 2, motor[i].omega_, motor[i].u_);
-      motor[i].omega = rk4(omega_dot, motor[i].omega_, t, h, 1, motor[i].i_);
-    }
-    drone.q = rk4(q_dot, drone.q_, t, h, 2, motor[RIGHT].omega_, motor[LEFT].omega_);
-    drone.theta = rk4(theta_dot, drone.theta_, t, h, 1, drone.q_);
-    drone.u = rk4(u_dot, drone.u_, t, h, 3, motor[RIGHT].omega_, motor[LEFT].omega_, drone.theta_);
-    drone.w = rk4(w_dot, drone.w_, t, h, 3, motor[RIGHT].omega_, motor[LEFT].omega_, drone.theta_);
-    drone.x = rk4(x_dot, drone.x_, t, h, 3, drone.u_, drone.w_, drone.theta_);
-    drone.z = rk4(z_dot, drone.z_, t, h, 3, drone.u_, drone.w_, drone.theta_);
+    motor.omega = rk4(omega_dot, motor.omega_, t, step, 0);
+    drone.w = rk4(w_dot, drone.w_, t, step, 1, motor.omega_);
+    drone.z = rk4(z_dot, drone.z_, t, step, 1, drone.w_);
 
-    t = t + h;
+    t = t + step;
     
     //Output
-    print_state(t, motor, drone);
+    print_state(t, &motor, drone);
   }
   cpu_time = clock() -cpu_time;
   //printf("elapsed time %f\n", (double)cpu_time/CLOCKS_PER_SEC);
 }
 
-void main(void)
+int main(void)
 {
   drone_sim();
 }
